9928c3821bda655675a00d181255f25810e020b0
chenzhenuaa <chenzhenuaa@gmail.com>
2012-12-13 15:45:06 +0800

    test case #86

---
+++ UITest/86_enter-advice-interface-off-line.js
@@ -0,0 +1,28 @@
+//用户已登录，且已申请过若干会员卡
+//用户处于离线状态（飞行模式）
+
+var target = UIATarget.localTarget();
+
+//查看会员卡信息并进入“建议与反馈”页面
+var traverseCells = function(target,collectionView){
+	for(var i=1,len=collectionView.cells().length;i<len;i+=1){
+		if(collectionView.cells()[i].checkIsValid()==0)  break;
+		collectionView.cells()[i].tap();
+		target.delay(1);
+		target.frontMostApp().mainWindow().collectionViews()[0].cells()[0].tap();
+		target.delay(1);
+		target.frontMostApp().mainWindow().collectionViews()[0].cells()[0].buttons()[1].tap();
+		target.delay(1);
+		target.tap({x:280.50,y:462.50});
+		/* 适用于iPad 2X
+		target.tap({x:654.00,y:964.00});
+		*/
+		target.delay(1);
+	}
+}
+
+UIALogger.logStart("查看会员卡信息并进入'建议与反馈'页面");
+
+traverseCells(target,target.frontMostApp().mainWindow().collectionViews()[0]);
+
+UIALogger.logPass("查看会员卡信息并进入'建议与反馈'页面");
