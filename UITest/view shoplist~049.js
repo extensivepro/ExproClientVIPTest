ed2a121b67f331ff3923877806e3d30abc8fc286
不二小猫 <chenqi19910514@gmail.com>
2012-12-19 16:02:03 +0800

    test case#049
    view shoplist

---
+++ UITest/view shoplist~049.js
@@ -0,0 +1,14 @@
+var target = UIATarget.localTarget();
+
+//商户列表
+UIALogger.logStart("查看商户列表");
+  	        target.delay(1);
+target.frontMostApp().mainWindow().buttons()[8].tap();
+		target.delay(1);
+target.frontMostApp().mainWindow().buttons()["Button"].tap();
+		target.delay(1);
+target.frontMostApp().mainWindow().buttons()["最新未选"].tap();
+		target.delay(1);
+target.frontMostApp().mainWindow().buttons()[8].tap();
+		target.delay(1);
+UIALogger.logPass("查看商户列表");
