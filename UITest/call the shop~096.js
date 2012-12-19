007b77eb7e59bc019032868dc91db4b201a6a1dc
cat0514 <chenqi19910514@gmail.com>
2012-12-19 16:17:51 +0800

    test case#096
    call the shop

--- UITest/call the shop~096.js
+++ UITest/call the shop~096.js
@@ -1,3 +1,21 @@
+var target=UIATarget.localTarget();
+
+UIATarget.onAlert=function onAlert(alert){
+  var title=alert.name();
+	// test if your script should handle the alert, and if so, return true
+	var info=alert.buttons()[0];
+	UIALogger.logDebug(info.name());
+	if(info.name()=="取消"){
+		alert.defaultButton().tap();
+		UIALogger.logDebug("拨打商户电话验证");
+		return true;
+	}
+	// add a warning to the log for each alert encountered
+	UIALogger.logWarning("Alert with title'" + title + "'encountered!");
+	// otherwise, return false to use the default handler
+	return false;
+}
+
 
 var target = UIATarget.localTarget();
 
@@ -17,3 +35,20 @@ target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["025896325
 target.delay(3)
 UIALogger.logPass("拨打商户电话");
 //拨打商户电话
+
+//var traverseCells = function(target,collectionView){
+//	for(var i=1,len=collectionView.cells().length;i<len;i+=2){
+//		if(collectionView.cells()[i].checkIsValid()==0)  break;
+//		collectionView.cells()[i].tap();
+//		target.delay(1);
+//		target.frontMostApp().mainWindow().collectionViews()[0].cells()[0].tap();
+//		target.delay(1);
+//		target.frontMostApp().mainWindow().collectionViews()[0].cells()[0].buttons()["商户"].tap();
+
+//		/*
+//		target.delay(1);
+//		target.tap({x:280.50,y:462.50});
+//		*/
+//		}
+//}
+
