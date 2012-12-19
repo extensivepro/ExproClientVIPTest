bd7501a2e24a329758a3074ac1dd80b01d167773
cat0514 <chenqi19910514@gmail.com>
2012-12-18 21:17:19 -0800

    test case #130
    view the shop list

---
+++ UITest/view the shoplist in the terminal~130.js
@@ -0,0 +1,19 @@
+
+var target = UIATarget.localTarget();
+
+//商户列表
+UIALogger.logStart("手机处于终端水平时查看商户列表");
+  	target.delay(1);
+target.frontMostApp().mainWindow().buttons()[8].tap();
+		target.delay(1);
+target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_LANDSCAPELEFT);
+		target.delay(1);
+target.frontMostApp().mainWindow().scrollViews()[0].tapWithOptions({tapOffset:{x:0.62, y:0.33}});
+		target.delay(1);
+target.frontMostApp().mainWindow().buttons()[0].tap();	
+		target.delay(1);	
+target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_PORTRAIT);
+		target.delay(1);
+target.frontMostApp().mainWindow().buttons()[8].tap();
+		target.delay(1);
+UIALogger.logPass("手机处于终端水平时查看商户列表");
